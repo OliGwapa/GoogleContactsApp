f31e9cb4ef538fbf11b16dd23aa9ed09fc7bee78
Blanche Alivio <alyssa.alivio@cit.edu>
2025-03-07 13:43:42 +0800

    Added Google Contacts API Integration

---
+++ src/test/java/com/Alivio/GoogleContactsApp/GoogleContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alivio.GoogleContactsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
